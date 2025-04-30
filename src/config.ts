gyasis/playwright-mcp

commit 4092d4e5a31186eae77dd9819c723a01e89ab1a5

chore: follow up to exposing playwright config options (#289)

src/config.ts
@@ -87,6 +87,7 @@ export async function configFromCLIOptions(cliOptions: CLIOptions): Promise<Conf
   const launchOptions: LaunchOptions = {
     channel,
     executablePath: cliOptions.executablePath,
+    headless: cliOptions.headless,
   };
 
   if (browserName === 'chromium')
@@ -126,7 +127,7 @@ async function loadConfig(configFile: string | undefined): Promise<Config> {
   try {
     return JSON.parse(await fs.promises.readFile(configFile, 'utf8'));
   } catch (error) {
-    throw new Error(`Failed to load config file: ${configFile}`);
+    throw new Error(`Failed to load config file: ${configFile}, ${error}`);
   }
 }
 

config.d.ts
@@ -30,6 +30,7 @@ export type Config = {
 
     /**
      * Path to a user data directory for browser profile persistence.
+     * Temporary directory is created by default.
      */
     userDataDir?: string;
 

src/tools/install.ts
@@ -29,7 +29,7 @@ const install = defineTool({
   },
 
   handle: async context => {
-    const channel = context.config.browser?.launchOptions?.channel ?? context.config.browser?.launchOptions.channel ?? context.config.browser?.launchOptions.browserName ?? 'chrome';
+    const channel = context.config.browser?.launchOptions?.channel ?? context.config.browser?.launchOptions.browserName ?? 'chrome';
     const cli = path.join(require.resolve('playwright/package.json'), '..', 'cli.js');
     const child = fork(cli, ['install', channel], {
       stdio: 'pipe',

tests/fixtures.ts
@@ -97,6 +97,7 @@ export const test = baseTest.extend<TestFixtures, WorkerFixtures>({
       `--no-first-run`,
       `--no-sandbox`,
       `--headless`,
+      '--use-mock-keychain',
       `data:text/html,hello world`,
     ], {
       stdio: 'pipe',

commit a7c961c65f262210999c8c92a4f69c7c6206ba3e

fix: default to headful (#305)
See https://github.com/microsoft/playwright-mcp/issues/304

Regressed in
https://github.com/microsoft/playwright-mcp/commit/4092d4e5a31186eae77dd9819c723a01e89ab1a5.

src/config.ts
@@ -89,7 +89,7 @@ export async function configFromCLIOptions(cliOptions: CLIOptions): Promise<Conf
   const launchOptions: LaunchOptions = {
     channel,
     executablePath: cliOptions.executablePath,
-    headless: cliOptions.headless,
+    headless: cliOptions.headless ?? false,
   };
 
   if (browserName === 'chromium')